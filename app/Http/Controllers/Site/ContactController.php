e73afd59ef6f14f519f289bba2d275e4701f672d
TARSO <tarso1156@gmail.com>
2021-09-14 19:56:35 -0300

    Add initial static routes

---
+++ app/Http/Controllers/Site/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("site.contact.index");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        ddd($request);
+    }
+}
